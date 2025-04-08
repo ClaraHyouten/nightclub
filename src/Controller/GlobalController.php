5dcf875d68101cd38e2df35ec4e74c973f08fd06
Clara Ortega <ortega.clara1331@gmail.com>
2025-04-08 09:56:10 +0200

    feat: initialization project and routes

--- src/Controller/GlobalController.php
+++ src/Controller/GlobalController.php
@@ -2,23 +2,58 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
-class GlobalController {
- #[Route("/")]
+use App\Form\ContactType;
+
+final class GlobalController extends AbstractController {
+ #[Route("/", name: 'home')]
+//  function index (WelcomeService $welcome): Response
  function index (): Response
  {
- return new Response('Hello world !');
+    return $this->render('pages/index.html.twig', [
+        'main_title' => 'Home',
+    ]);
  }
- #[Route("/contact")]
- function contact (): Response
+ 
+ #[Route("/contact", name: 'contact')]
+ function contact (Request $request): Response
  {
- return new Response('Me contacter');
+    $form = $this->createForm(ContactType::class);
+    // ->getForm();
+    $form->handleRequest($request);
+
+    return $this->render('pages/contact.html.twig', [
+        'main_title' => 'Contact',
+        'form' => $form,
+    ]);
  }
- #[Route("/about")]
+ 
+ #[Route("/about", name: 'about')]
  function about (): Response
  {
- return new Response('A propos');
- }
+    $steps_key = [
+        ['year' => '1978', 'description' => 'Ouverture du nightclub'],
+        ['year' => '2000', 'description' => 'Ouverture de l\'espace VIP'],
+        ['year' => '2010', 'description' => 'Ouverture de la salle de concert'],
+        ['year' => '2015', 'description' => 'Ouverture de la salle de jeux'],
+        ['year' => '2020', 'description' => 'Déménagement dans les locaux actuels'],
+    ];
+    // dd($steps_key);
+    return $this->render('pages/about.html.twig', [
+        'main_title' => 'About',
+        'steps_key' => $steps_key,
+    ]);
+}
+
+ #[Route("/404", name: '404')]
+ function error_404 (): Response
+ {
+    return $this->render('pages/404.html.twig', [
+        'main_title' => 'Error 404',
+    ]);
+}
 }
